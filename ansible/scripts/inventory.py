cd8a2a64ea08671c1d8290776e5acf11207d06d2
kylerisse <kylerisse@gmail.com>
2018-02-15 15:51:54 -0800

    functional import of switches, vlans, and server

--- ansible/scripts/inventory.py
+++ ansible/scripts/inventory.py
@@ -3,21 +3,32 @@
 # Dynamic inventory script used to slurp in various 
 # SCaLE specific text files to produce a sane inventory to ansible
 
+# use of OOP, encapsulation, and non-standard libraries is purposly omitted to aide 
+# in ease of support; "global" list variable are used with a dedicated function 
+# for each 
+
 import os
 import re
 
 # constants with locations to files or directories within the repo
 vlansddir = "../../switch-configuration/config/vlans.d/"
-serverfile = "../../facts/servers/serverlist.csv"
+switchesfile = "../../switch-configuration/config/switchtypes"
+serverfile = "../../facts/servers/serverlist.tsv"
 
 # globals
 #
 # vlans = []{name, id, ipv6prefix, ipv6bitmask, ipv4prefix, ipv4bitmask, building, description}
 vlans = []
-# servers = []{name, mac-address, ipv4, ipv6}
+# switches = []{name, ipv6address}
+switches = []
+# servers = []{name, mac-address, ipv6, ipv4, ansiblerole, vlanname}
 servers = []
+# listinv = {group: {hosts: [], vars: {var1: x, var2: y}}}
+listinv = {}
+# hostinv = {host: {var1: x, var2: y}}
+hostinv = {}
 
-# populatevlans() will populate the vlans dictionary
+# populatevlans() will populate the vlans list
 def populatevlans():
     filelist = (os.listdir(vlansddir))
     for file in filelist:
@@ -48,9 +59,52 @@ def populatevlans():
                     "description": desc,
                 })
 
+def populateswitches():
+    f = open(switchesfile, 'r')
+    flines = f.readlines()
+    f.close()
+    for line in flines:
+        if not (line[0] == '/' or line[0] == ' ' or line[0] == '\n'):
+            elems = re.split(r'\t+', line)
+            name = elems[0]
+            ipv6 = elems[3]
+            switches.append({
+                "name": name,
+                "ipv6": ipv6,
+            })
+
+def populateservers():
+    f = open(serverfile, 'r')
+    flines = f.readlines()
+    f.close()
+    for line in flines:
+        if not (line[0] == '/' or line[0] == ' ' or line[0] == '\n'):
+            elems = re.split(r'\t+', line)
+            if len(elems) > 2:
+                name = elems[0]            
+                mac = elems[1]
+                ipv6 = elems[2] 
+                ipv4 = elems[3]
+                for v in vlans:
+                    if ipv6.find(v["ipv6prefix"]) > -1:
+                        vlan = v["name"]
+                ans = elems[4].split('\n')[0]
+                servers.append({
+                    "name": name,
+                    "macaddress": mac,
+                    "ipv6": ipv6,
+                    "ipv4": ipv4,
+                    "ansiblerole": ans,
+                    "vlan": vlan,
+                })
+
 def Main():
     populatevlans()
     print(vlans)
+    populateswitches()
+    print(switches)
+    populateservers()
+    print(servers)
 
 if __name__ == "__main__":
     Main()
