45a539d120a9b39768699e589e2e1c61b2afdae2
Robert James Hernandez <rob@sarcasticadmin.com>
2019-01-11 23:54:24 -0800

    Adding serverspec and deps
    Adding initial serverspec tests for openwrt AP imgs. This includes
    the Rakefile for targeting a specific host.

---
+++ tests/serverspec/spec/openwrt/common_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+DEFAULT_PKGS=["apinger", "bash", "logrotate",
+              "python3", "rsyslogd", "zabbix_agentd"]
+
+DEFAULT_PKGS.each do |pkg|
+  describe command("which #{pkg} 2> /dev/null") do
+    its(:exit_status) { should eq 0 }
+  end
+end
+
+# Validate rsyslogd config
+# TODO: After fixing issue #125
+#describe command('rsyslogd -N1') do
+#  its(:exit_status) { should eq 0 }
+#end
+
+describe file('/root/bin/wifi-details.sh') do
+    it { should exist }
+    it { should be_mode 750 }
+    it { should be_owned_by 'root' }
+    it { should be_grouped_into 'root' }
+end
