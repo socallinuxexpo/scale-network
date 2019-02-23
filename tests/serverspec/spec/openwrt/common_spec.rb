d50b57c80b97bed757133e293081600e266858c1
Robert James Hernandez <rob@sarcasticadmin.com>
2019-02-23 01:12:32 -0800

    Fixing zabbix_agentd config for syslog

--- tests/serverspec/spec/openwrt/common_spec.rb
+++ tests/serverspec/spec/openwrt/common_spec.rb
@@ -3,8 +3,16 @@ require 'spec_helper'
 DEFAULT_PKGS=["apinger", "bash", "logrotate",
               "python3", "rsyslogd", "zabbix_agentd"]
 
+DEFAULT_SERVICES=["apinger", "crond", "rsyslogd", "zabbix"]
+
 DEFAULT_PKGS.each do |pkg|
   describe command("which #{pkg} 2> /dev/null") do
+    its(:exit_status) { should eq 0 }
+  end
+end
+
+DEFAULT_SERVICES.each do |service|
+  describe command("pgrep #{service}") do
     its(:exit_status) { should eq 0 }
   end
 end
