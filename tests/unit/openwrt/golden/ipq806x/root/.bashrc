699e43a8ba3d42ece1e985a9dbe645031bc81503
Robert James Hernandez <rob@sarcasticadmin.com>
2019-03-05 05:03:16 +0000

    Updating openwrt golden tests

---
+++ tests/unit/openwrt/golden/ipq806x/root/.bashrc
@@ -0,0 +1,4 @@
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+export TERM="vt100"
--- tests/unit/openwrt/golden/ipq806x/root/bin/wifi-details.sh
+++ tests/unit/openwrt/golden/ipq806x/root/bin/wifi-details.sh
@@ -4,11 +4,12 @@ while true
 do
   # Example of output
   # /sys/kernel/debug/ieee80211/phy1/netdev:wlan1/stations/14:1a:a3:9a:47:35/rc_stats
-  CLIENTS=($(find /sys/kernel/debug/ieee80211/ -name rc_stats))
-  for CLIENT in ${CLIENTS[@]}; do
+  CLIENTS="$(find /sys/kernel/debug/ieee80211/ -name rc_stats)"
+  for CLIENT in ${CLIENTS}; do
     echo $CLIENT |sed -e s#/sys/kernel/debug/ieee80211/## -e s#/netdev:#' '# -e s#/stations/#' '# -e s#/rc_stats## -e s/:/-/g |while read phy net mac
     do
-      /usr/bin/logger -t "stats-$net-$mac" $(cat $CLIENT |sed -e :0 -e '/,.*,.*,/b' -e N -e 's/\n/#014/' -e b0)
+      # Loggin as local7 to skip /var/log/messages
+      /usr/bin/logger -p local7.info -t "stats-$net-$mac" $(cat $CLIENT |sed -e :0 -e '/,.*,.*,/b' -e N -e 's/\n/#014/' -e b0)
     done
   done
   # Wait a little
