1adee6b471216a8efbf9b315f3acb82630309cc0
Robert James Hernandez <rob@sarcasticadmin.com>
2019-07-30 17:45:23 -0700

    Regenerate goldens file in openwrt tests
    Since we are moving the config files to have a .x suffix then
    golden files in the test suite needed to be updated to reflect this.
    Also including new versions of config-version.sh for golden config

--- tests/unit/openwrt/golden/ar71xx/root/bin/config-version.sh
+++ tests/unit/openwrt/golden/ar71xx/root/bin/config-version.sh
@@ -3,19 +3,28 @@
 usage(){
   cat << EOF
 usage: "$(basename "$0")" [OPTIONS] ARGS
+
 Set OpenWRT config to specific version
+
 OPTIONS:
   -h      Show this message
-  -c      Configuration version [default: 0]
+  -c      Configuration version
+
 EXAMPLES:
+  Print out the current config version
+      "$(basename "$0")"
+
   To set config version 1:
       "$(basename "$0")" -c 1
+
 EOF
 }
 
-CONFVER=0
+CONFVER=''
+# Current files being managed via this script
 FILES=(wireless network)
 CONFROOT='/etc/config'
+CONFLIST=()
 
 while getopts "h:c:" OPTION
 do
@@ -35,21 +44,54 @@ do
 done
 shift $((OPTIND-1))
 
+build_conflist(){
+  local prevver=''
+  for file in ${FILES[*]}
+  do
+    # Make sure existing config is a symlink
+    p1=$(readlink $CONFROOT/$file)
+    if [ "$?" -eq 0 ]; then
+      currver=$(echo $p1 | awk -F. '{print $NF}')
+      if [ "$prevver" == '' ] || [ "$prevver" -eq "$currver" ]; then
+        CONFLIST+=( "$p1" )
+        prevver=$currver
+      else
+        echo "$CONFROOT/${p1} doesnt match other config versions: ${CONFLIST[@]}"
+        exit 1
+      fi
+    else
+      echo "No symlink exists for $CONFROOT/$file"
+      exit 1
+    fi
+
+    # exit if the target config version doesn't exist
+    if [ "$CONFVER" != "" ] && [ ! -f "$CONFROOT/$file.$CONFVER" ]
+    then
+      echo "no file $CONFROOT/$file.$CONFVER"
+      exit 1
+    fi
+  done
+}
+
+build_conflist
+
+# If config ver is not passed then print out current version and exit
+if [ "$CONFVER" == "" ]; then
+   echo ${CONFLIST[0]} |  awk -F. '{print $NF}'
+   exit 0
+fi
+
 RELOAD=0
 
 for file in ${FILES[*]}
 do
-  # exit if the target version doesn't exist
-  if [ ! -f "$CONFROOT/$file.$CONFVER" ]
-  then
-    echo "no file $CONFROOT/$file.$CONFVER"
-    exit 1
-  fi
   # create symlinks if the current version isn't active
-  if [ ! -L "$CONFROOT/$file" ] || [ ! "$(readlink $CONFROOT/$file)" = "$CONFROOT/$file.$CONFVER" ]
+  if [ ! -L "$CONFROOT/$file" ] || [ ! "$(readlink $CONFROOT/$file)" == "$file.$CONFVER" ]
   then
     rm -f $CONFROOT/$file
-    ln -s $CONFROOT/$file\.$CONFVER $CONFROOT/$file
+    # Keep this link relative since that mirrors how its done during build time
+    # subshell also allows us not to worry about pwd
+    (cd $CONFROOT && ln -s $file\.$CONFVER $file)
     RELOAD=1
   fi
 done
@@ -58,4 +100,6 @@ done
 if [ $RELOAD -eq 1 ]; then
     /etc/init.d/network restart
     /sbin/wifi reload
+else
+  echo "no change to config symlinks required"
 fi
