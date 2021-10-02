940c262eb8124ce1c2c0dcba5025d0afb72f74c3
Robert James Hernandez <rob@sarcasticadmin.com>
2021-10-02 16:47:22 -0700

    finally adding nixpkg toplevel

---
+++ shell.nix
@@ -0,0 +1,18 @@
+# TODO: Pin this to a specific version of nixpkgs
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+
+let
+  scale_python = python38.withPackages
+      (pythonPackages: with pythonPackages; [ pytest pylint ]);
+
+  # Trying to keep these pkg sets separate for later
+  global = [ bash curl git jq kermit  screen ] ++ [ scale_python ];
+  ansible_sub = [ansible_2_11 ansible-lint];
+  openwrt_sub = [ expect gomplate magic-wormhole tftp-hpa ];
+  network_sub = [ perl532 ];
+in
+mkShell {
+  buildInputs = [ global ] ++ [ ansible_sub ] ++ [ openwrt_sub ] ++ [ network_sub ];
+}
