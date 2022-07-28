4242e5ccba8a42dd4a282c9ddc35105fd1bfc484
Jim Dennis <answrguy@gmail.com>
2022-07-28 13:00:07 -0700

    use set; and in returns boolean

--- facts/datasource.py
+++ facts/datasource.py
@@ -29,14 +29,12 @@ def isvalidmodel(model):
     '''
     test for valid switch model (enumerated)
     '''
-    if model in [
+    return model in {
             "ex4200-48p", "ex4200-48t",
             "ex4200-24p", "ex4200-24t",
             "ex2200-48p", "ex2200-48t",
             "ex2200-24p", "ex2200-24t"
-        ]:
-        return True
-    return False
+        }
 
 def isvalidip(addr):
     '''test for valid v4 or v6 ip'''
