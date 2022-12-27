546a3c12efb898fceae036763175c2873abce138
Robert James Hernandez <rob@sarcasticadmin.com>
2022-12-27 05:28:42 +0000

    fixing existing fmt errors in *.nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,15 @@
 # Leverage the nix flake devShell to get pinned nixpkgs
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 with pkgs;
 
 let
   scale_python = python38.withPackages
-      (pythonPackages: with pythonPackages; [ pytest pylint ]);
+    (pythonPackages: with pythonPackages; [ pytest pylint ]);
 
   # Trying to keep these pkg sets separate for later
   global = [ bash curl git jq kermit nixpkgs-fmt treefmt screen glibcLocales ] ++ [ scale_python ];
-  ansible_sub = [ansible_2_12 ansible-lint];
+  ansible_sub = [ ansible_2_12 ansible-lint ];
   openwrt_sub = [ expect gomplate magic-wormhole tftp-hpa nettools unixtools.ping iperf3 ncurses ncurses.dev pkg-config gcc stdenv ];
   network_sub = [ perl534 ];
 in
