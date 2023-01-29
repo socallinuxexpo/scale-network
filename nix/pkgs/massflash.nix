3f68ae6357f05731a6b9acbe7569c3102d7fdd9b
Robert James Hernandez <rob@sarcasticadmin.com>
2023-01-29 17:49:59 +0000

    init massflash livecd and nixpkgs

---
+++ nix/pkgs/massflash.nix
@@ -0,0 +1,21 @@
+{ stdenvNoCC, copyPathsToStore, lib, expect, openssh }:
+let
+  local_manifests = copyPathsToStore [
+    ../../openwrt/scripts/massflash/massflash
+  ];
+in
+stdenvNoCC.mkDerivation {
+  name = "massflash";
+
+  propagatedBuildInputs = [
+    bash
+    openssh
+  ];
+
+  buildCommand = ''
+    mkdir -p $out/bin
+    for local_manifest in ${lib.concatMapStringsSep " " toString local_manifests}; do
+      cp -r $local_manifest $out/bin/$(stripHash $local_manifest; echo $strippedName)
+    done
+  '';
+}
