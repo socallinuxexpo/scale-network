5b75a5a2bb9db08df290cafffeed4272d93d8ec6
Robert James Hernandez <rob@sarcasticadmin.com>
2023-02-08 09:15:39 +0000

    core role ipv4/v6 static addressing

--- nix/machines/core/default.nix
+++ nix/machines/core/default.nix
@@ -19,6 +19,26 @@ in
     };
   };
 
+  networking.usePredictableInterfaceNames = false;
+  # Make sure that the makes of these files are actually lexicographically before 99-default.link provides by systemd defaults since first match wins
+  # Ref: https://github.com/systemd/systemd/issues/9227#issuecomment-395500679
+  systemd.network = {
+    enable = true;
+    links = {
+      "10-lan" = {
+        matchConfig = { OriginalName = "*"; };
+        linkConfig = { MACAddress = "58:9c:fc:00:38:5f"; MACAddressPolicy = "none"; };
+      };
+    };
+    networks = {
+      "10-lan" = {
+        address = [ "10.0.3.5/24" "2001:470:f0fb:103::5/64" ];
+        gateway = [ "10.0.3.1" ];
+        matchConfig = { Name = "eth0"; };
+      };
+    };
+  };
+
   security.sudo.wheelNeedsPassword = false;
 
   environment.systemPackages = with pkgs; [
