b48dddf4f3696fa6958553a71102a309d242c343
Robert James Hernandez <rob@sarcasticadmin.com>
2023-02-23 08:52:33 +0000

    Adding actual client/server dhcpd test for core

--- nix/tests/core.nix
+++ nix/tests/core.nix
@@ -1,14 +1,41 @@
 {
   name = "core";
-  nodes.machine1 = { ... }: { imports = [ ../machines/core ]; } // {
-    virtualisation.graphics = false;
+  nodes = {
+    coreServer = { lib, ... }: {
+      imports = [ ../machines/core ];
+    } // {
+      virtualisation.vlans = [ 1 ];
+      virtualisation.graphics = false;
+      systemd.network = {
+        networks = lib.mkForce {
+          "01-eth1" = {
+            name = "eth1";
+            enable = true;
+            address = [ "10.0.3.5/24" ];
+            gateway = [ "10.0.3.1" ];
+          };
+        };
+      };
+    };
+
+    client1 = { config, pkgs, ... }: {
+      virtualisation.vlans = [ 1 ];
+      systemd.services.systemd-networkd.environment.SYSTEMD_LOG_LEVEL = "debug";
+      networking = {
+        useNetworkd = true;
+        useDHCP = false;
+        firewall.enable = false;
+        interfaces.eth1.useDHCP = true;
+      };
+    };
   };
 
   testScript = ''
     start_all()
     # Kea needs a sec to startup so well sleep
-    machine1.succeed("sleep 10")
-    machine1.succeed("kea-dhcp4 -t /etc/kea/dhcp4-server.conf")
-    machine1.succeed("systemctl is-active kea-dhcp4-server")
+    coreServer.succeed("sleep 10")
+    coreServer.succeed("kea-dhcp4 -t /etc/kea/dhcp4-server.conf")
+    client1.wait_for_unit("systemd-networkd-wait-online.service")
+    client1.wait_until_succeeds("ping -c 5 10.0.3.5")
   '';
 }
