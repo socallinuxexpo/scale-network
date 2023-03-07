1d9e22b19a56e30639c5a9e10bc74356a1a999af
Robert James Hernandez <rob@sarcasticadmin.com>
2023-03-07 19:37:27 +0000

    setting users to immutable and hash for root user

--- nix/machines/core/default.nix
+++ nix/machines/core/default.nix
@@ -6,7 +6,8 @@
 
   boot.kernelParams = [ "console=ttyS0" ];
 
-  users.extraUsers.root.password = "";
+  users.mutableUsers = false;
+  users.extraUsers.root.hashedPassword = "$6$3Hm/K5fbR3UEMK6H$3aaegtdwvejGk9Bk0ttN5bNJn4z2Yt6LWXD3nGI7.44Pbm7A1TpKuxG9XQLwsj7M9NEk8eB5Exg0qVRV//6br/";
 
   users.users = {
     rherna = {
