19490b8d1d5fc00845678ab96b596faa43c0bd9f
Robert James Hernandez <rob@sarcasticadmin.com>
2023-12-16 18:24:49 +0000

    adding owen to users and cleaning up robs name

--- nix/machines/_common/users.nix
+++ nix/machines/_common/users.nix
@@ -5,12 +5,19 @@
   users.extraUsers.root.hashedPassword = "$6$3Hm/K5fbR3UEMK6H$3aaegtdwvejGk9Bk0ttN5bNJn4z2Yt6LWXD3nGI7.44Pbm7A1TpKuxG9XQLwsj7M9NEk8eB5Exg0qVRV//6br/";
 
   users.users = {
-    rherna = {
+    rob = {
       isNormalUser = true;
       uid = 2005;
       extraGroups = [ "wheel" ];
       openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMEiESod7DOT2cmT2QEYjBIrzYqTDnJLld1em3doDROq" ];
     };
+    owen = {
+      isNormalUser = true;
+      uid = 2006;
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [ "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBjjcUJLTENGrV6K/nrPOswcBVMMuS4sLSs0UyTRw8wU87PDUzJz8Ht2SgHqeEQJdRm1+b6iLsx2uKOf+/pU8qE= root@kiev.delong.com" ];
+    };
+
   };
 
 }
