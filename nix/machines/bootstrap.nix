0854e1f8fa3759a4314d3d41e9ecebcb3830f9ff
Robert James Hernandez <rob@sarcasticadmin.com>
2023-12-18 05:51:44 +0000

    bonding links for iso

--- nix/machines/bootstrap.nix
+++ nix/machines/bootstrap.nix
@@ -11,8 +11,50 @@
 
   networking = {
     firewall.enable = true;
-    useDHCP = true;
-    enableIPv6 = true;
+  };
+
+  systemd.network = {
+    netdevs = {
+      "10-bond0" = {
+        netdevConfig = {
+          Kind = "bond";
+          Name = "bond0";
+        };
+        bondConfig = {
+          Mode = "802.3ad";
+          LACPTransmitRate = "fast";
+          TransmitHashPolicy = "layer3+4";
+        };
+      };
+    };
+    networks = {
+      "30-eno1" = {
+        matchConfig.Name = "eno1";
+        networkConfig.Bond = "bond0";
+      };
+      "30-eno2" = {
+        matchConfig.Name = "eno2";
+        networkConfig.Bond = "bond0";
+      };
+      "40-eno3" = {
+        matchConfig.Name = "eno3";
+        networkConfig.Bond = "bond0";
+      };
+      "50-eno4" = {
+        matchConfig.Name = "eno4";
+        networkConfig.Bond = "bond0";
+      };
+      "60-bond0" = {
+        matchConfig.Name = "bond0";
+        linkConfig = {
+          RequiredForOnline = "carrier";
+        };
+        networkConfig.LinkLocalAddressing = "no";
+        networkConfig = {
+          DHCP = "ipv4";
+        };
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
