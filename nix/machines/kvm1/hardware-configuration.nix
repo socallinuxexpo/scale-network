405e0a8dc47697d20f532858ccd0b97fa7c6bfbc
Robert James Hernandez <rob@sarcasticadmin.com>
2024-01-13 20:31:36 +0000

    missing kernel mods wtf

--- nix/machines/kvm1/hardware-configuration.nix
+++ nix/machines/kvm1/hardware-configuration.nix
@@ -6,7 +6,7 @@
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "mpt3sas" "megaraid_sas" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
