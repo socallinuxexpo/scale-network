5db482dc83cc5c348ec083be3874d95e28a710af
Robert James Hernandez <rob@sarcasticadmin.com>
2024-01-14 23:24:27 +0000

    facts: include ex4200-48px as valid switch_type

--- facts/datasource.py
+++ facts/datasource.py
@@ -32,7 +32,8 @@ def isvalidmodel(model):
         "ex4200-48p", "ex4200-48t",
         "ex4200-24p", "ex4200-24t",
         "ex2200-48p", "ex2200-48t",
-        "ex2200-24p", "ex2200-24t"
+        "ex2200-24p", "ex2200-24t",
+        "ex4200-48px"
     }
 
 def isvalidip(addr):
