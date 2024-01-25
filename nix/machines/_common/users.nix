67264ca6013670b29571abfd8e862b018d80390e
Robert James Hernandez <rob@sarcasticadmin.com>
2024-01-25 05:42:19 +0000

    nix: init owens user and key
    temporarily add owens user to the default keys for nixos machines. There
    will be a follow up PR to actually do this via facts but this will do
    for now so we can get the devServer and kvms instances online.

--- nix/machines/_common/users.nix
+++ nix/machines/_common/users.nix
@@ -11,6 +11,13 @@
       extraGroups = [ "wheel" ];
       openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMEiESod7DOT2cmT2QEYjBIrzYqTDnJLld1em3doDROq" ];
     };
+    owen = {
+      isNormalUser = true;
+      uid = 2006;
+      extraGroups = [ "wheel" ];
+      openssh.authorizedKeys.keys = [ "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBBjjcUJLTENGrV6K/nrPOswcBVMMuS4sLSs0UyTRw8wU87PDUzJz8Ht2SgHqeEQJdRm1+b6iLsx2uKOf+/pU8qE= root@kiev.delong.com" ];
+    };
+
   };
 
 }
