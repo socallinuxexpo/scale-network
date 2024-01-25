0f6e8e0bc55810abf221af0ba18cb0d188fb8697
Robert James Hernandez <rob@sarcasticadmin.com>
2024-01-25 05:42:06 +0000

    nix: rherna to rob as username

--- nix/machines/_common/users.nix
+++ nix/machines/_common/users.nix
@@ -5,7 +5,7 @@
   users.extraUsers.root.hashedPassword = "$6$3Hm/K5fbR3UEMK6H$3aaegtdwvejGk9Bk0ttN5bNJn4z2Yt6LWXD3nGI7.44Pbm7A1TpKuxG9XQLwsj7M9NEk8eB5Exg0qVRV//6br/";
 
   users.users = {
-    rherna = {
+    rob = {
       isNormalUser = true;
       uid = 2005;
       extraGroups = [ "wheel" ];
