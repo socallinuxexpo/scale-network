2db543650205dc419bc20ba3799fac35a782d64a
Berkhan Berkdemir <berkhan@tekdanisman.com>
2024-03-15 16:26:08 -0700

    Only access to the monitoring machine

--- nix/machines/_common/users.nix
+++ nix/machines/_common/users.nix
@@ -37,11 +37,7 @@
       extraGroups = [ "wheel" ];
       openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMlEPbMnefiPfCTKb9lOzPzfnOVAohO08myWWMm9EJxZ" ];
     };
-    berkhan = {
-      isNormalUser = true;
-      uid = 2010;
-      extraGroups = [ "wheel" ];
-      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH6UhZ/oPqiFzCOxoZWeUqeGZCVLLNQbHH3uuIa6PCTz" ]; 
+
   };
 
 }
