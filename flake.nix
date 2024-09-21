95e8069965b4a27df9eb6eedabf5028996cb3afb
Daniel Baker <daniel.n.baker@gmail.com>
2024-09-21 14:31:03 -0700

    flake-inputs: add nixpkgs-unstable; add treefmt-mnix
    Added treefmt-nix to handle formatting all files in the repo. The
    current pin of nixpkgs is not ready to be bumped so a second input was
    added.

--- flake.nix
+++ flake.nix
@@ -5,6 +5,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts = { url = "github:hercules-ci/flake-parts"; inputs.nixpkgs-lib.follows = "nixpkgs"; };
     microvm = { url = "github:sarcasticadmin/microvm.nix/rh/1707108673virtio"; inputs.nixpkgs.follows = "nixpkgs"; inputs.spectrum.follows = ""; }; # Currently using this fork since the upstream seems to be causing an issue
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
   };
 
 
