63f0957918c7a623131e4fc6492e7229f9989203
Robert James Hernandez <rob@sarcasticadmin.com>
2024-10-10 20:02:12 +0000

    derp

---
+++ nix/machines/devServer/gitlab.nix
@@ -0,0 +1,14 @@
+{ config, ...}:
+{
+  services.gitlab-runner = {
+    enable = true;
+    gracefulTermination = true;
+    services = 
+      shell = {
+        authenticationTokenConfigFile = /persist/etc/gitlab/shellAuthenticationToken.env;
+        executor = "shell";
+        tagList = [ "shell" ];
+      };
+    };
+  };
+}
