da8d51f700046ddb318fc72a07bd4e7fac2b2d05
Daniel Baker <daniel.n.baker@gmail.com>
2024-11-24 20:03:07 -0800

    nix.flake.inputs: cleanup
    Flatten the flake inputs. Makes it easier to see all inputs at once when
    they are sorted.

--- flake.nix
+++ flake.nix
@@ -2,26 +2,19 @@
   nixConfig.bash-prompt = "\\[\\033[01;32m\\][nix-flakes \\W] \$\\[\\033[00m\\] ";
 
   inputs = {
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs-2405";
-    };
-    microvm = {
-      url = "github:sarcasticadmin/microvm.nix/rh/1707108673virtio";
-      inputs.nixpkgs.follows = "nixpkgs-2405";
-      inputs.spectrum.follows = "";
-    }; # Currently using this fork since the upstream seems to be causing an issue
+    disko.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    disko.url = "github:nix-community/disko/e55f9a8678adc02024a4877c2a403e3f6daf24fe";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs-2405";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    microvm.inputs.nixpkgs.follows = "nixpkgs-2405";
+    microvm.inputs.spectrum.follows = "";
+    # Currently using this fork since the upstream seems to be causing an issue
+    microvm.url = "github:sarcasticadmin/microvm.nix/rh/1707108673virtio";
     nixpkgs-2405.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs.follows = "nixpkgs-2405"; # get rid of this once flake parts is gone
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
-    disko = {
-      url = "github:nix-community/disko/e55f9a8678adc02024a4877c2a403e3f6daf24fe";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
+    nixpkgs.follows = "nixpkgs-2405"; # get rid of this once flake parts is gone
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
@@ -34,6 +27,7 @@
       imports = [ ./nix/flake-module.nix ];
     })
     // {
+      devShells = import ./nix/dev-shells inputs;
       formatter = import ./nix/formatter inputs;
       formatterModule = import ./nix/formatterModule inputs;
       legacyPackages = import ./nix/legacy-packages inputs;
