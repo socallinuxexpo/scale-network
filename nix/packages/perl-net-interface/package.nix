6adb4e310f12b950d7f6218f9fe22a582b1108e4
Robert James Hernandez <rob@sarcasticadmin.com>
2024-12-01 17:31:15 +0000

    packages.perlNetInterface: include which in buildInputs
    Without `which` the resulting error happens at build time:
    
    ...
    checking for LOCAL_SIZEOF_SOCKADDR_OSITP... not defined
    configure: creating ./config.status
    config.status: creating config.h
    Can't exec "which": No such file or directory at inst/netsymbols.pl line 55 (#1)
        (W exec) A system(), exec(), or piped open call could not execute the
        named program for the indicated reason.  Typical reasons include: the
        permissions were wrong on the file, the file wasn't found in
        $ENV{PATH}, the executable in question was compiled for another
        architecture, or the #! line in a script points to an interpreter that
        can't be run for similar reasons.  (Or maybe your system doesn't support
        #! at all.)
    
    Use of uninitialized value $grep in pattern match (m//) at inst/netsymbols.pl
            line 56 (#1)
        (W uninitialized) An undefined value was used as if it were already
        defined.  It was interpreted as a "" or a 0, but maybe it was a mistake.
        To suppress this warning assign a defined value to your variables.
    
        To help you figure out what was undefined, perl will try to tell you
        the name of the variable (if any) that was undefined.  In some cases
        it cannot do this, so it also tells you what operation you used the
        undefined value in.  Note, however, that perl optimizes your program
        and the operation displayed in the warning may not necessarily appear
        literally in your program.  For example, "that $foo" is usually
        optimized into "that " . $foo, and the warning will refer to the
        concatenation (.) operator, even though there is no . in
        your program.
    
    Use of uninitialized value $grep in concatenation (.) or string at
            inst/netsymbols.pl line 60 (#1)
    sh: illegal option -r
    sh: illegal option -r
    sh: illegal option -r
    Uncaught exception from user code:
            AF_INET not found in netsymbolsC.inc,
            prerequisite development library header files missing from /usr/include/sys

--- nix/packages/perl-net-interface/package.nix
+++ nix/packages/perl-net-interface/package.nix
@@ -2,12 +2,13 @@
   perlPackages,
   fetchurl,
   linuxHeaders,
+  which
 }:
 
 perlPackages.buildPerlPackage {
   pname = "NetInterface";
   version = "1.016";
-  buildInputs = [ linuxHeaders ];
+  buildInputs = [ linuxHeaders which ];
   doCheck = false;
   src = fetchurl {
     url = "mirror://cpan/authors/id/M/MI/MIKER/Net-Interface-1.016.tar.gz";
