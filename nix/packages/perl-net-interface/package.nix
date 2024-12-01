0290733bb867a728e4f9c589258c9208e350fae9
Robert James Hernandez <rob@sarcasticadmin.com>
2024-12-01 17:50:06 +0000

    trying to refer to headers during build

--- nix/packages/perl-net-interface/package.nix
+++ nix/packages/perl-net-interface/package.nix
@@ -10,6 +10,18 @@ perlPackages.buildPerlPackage {
   version = "1.016";
   buildInputs = [ linuxHeaders which ];
   doCheck = false;
+
+  CPPFLAGS="-Iusr/include";
+
+  postPatch = ''
+    mkdir -p usr/include/sys/
+    cp -pr ${linuxHeaders}/include usr/
+    ln -s ${linuxHeaders}/include/linux/socket.h usr/include/sys/socket.h
+    ls -lahR usr/
+    echo "aaaaaaaa"
+    echo $CPPFLAGS
+  '';
+
   src = fetchurl {
     url = "mirror://cpan/authors/id/M/MI/MIKER/Net-Interface-1.016.tar.gz";
     hash = "sha256-e+RGk14BPQ7dPTcfkvuQo7s4q+mVYoidgXiVMSnlNQg=";
