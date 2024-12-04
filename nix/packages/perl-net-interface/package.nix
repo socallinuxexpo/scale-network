3179981d34834933f1886d1f04a8d50e7698b732
Daniel Baker <daniel.n.baker@gmail.com>
2024-12-03 21:01:42 -0800

    packages.perlNetInterface: patch grepping for library headers
    The make file has a check to grep for the AF_INET symbol in
    /usr/include/sys. Patching this out so that it builds. Not certain if
    this will cause runtime failures.

--- nix/packages/perl-net-interface/package.nix
+++ nix/packages/perl-net-interface/package.nix
@@ -1,29 +1,17 @@
 {
   perlPackages,
   fetchurl,
-  linuxHeaders,
-  which
 }:
-
 perlPackages.buildPerlPackage {
   pname = "NetInterface";
   version = "1.016";
-  buildInputs = [ linuxHeaders which ];
-  doCheck = false;
-
-  CPPFLAGS="-Iusr/include";
-
-  postPatch = ''
-    mkdir -p usr/include/sys/
-    cp -pr ${linuxHeaders}/include usr/
-    ln -s ${linuxHeaders}/include/linux/socket.h usr/include/sys/socket.h
-    ls -lahR usr/
-    echo "aaaaaaaa"
-    echo $CPPFLAGS
-  '';
 
   src = fetchurl {
     url = "mirror://cpan/authors/id/M/MI/MIKER/Net-Interface-1.016.tar.gz";
     hash = "sha256-e+RGk14BPQ7dPTcfkvuQo7s4q+mVYoidgXiVMSnlNQg=";
   };
+
+  patches = [ ./net-interface-remove-grep-af-inet.patch ];
+
+  doCheck = false;
 }
