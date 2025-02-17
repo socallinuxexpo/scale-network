cfc646f71d7d7dfa050829a672017572a8684358
jeffreytjung <80990772+jeffreytjung@users.noreply.github.com>
2025-02-16 21:41:20 -0800

    Update configuration.nix with Loki Service
    This is the Loki service for ingesting logs for the monitoring section of the infrastrucutre.
    
    This change adds a loki-config.yaml file.
    
    The commit does not have Loki fully functioning.

--- nix/nixos-configurations/monitor/configuration.nix
+++ nix/nixos-configurations/monitor/configuration.nix
@@ -102,7 +102,36 @@ in
         ];
       };
     };
+    
+    loki = {
+      enable = true;
+      configFile = pkgs.writeText "loki-config.yaml" ''
+        auth_enabled: false
+      server:
+        http_listen_port: 3100
 
+      common:
+        ring:
+          instance_addr: 127.0.0.1
+          kvstore:
+            store: inmemory
+        replication_factor: 1
+        path_prefix: /tmp/loki
+
+      schema_config:
+        configs:
+          - from: 2020-05-15
+            store: tsdb
+            object_store: filesystem
+            schema: v13
+            index:
+              prefix: index_
+              period: 24h
+      storage_config:
+        filesyste:
+          directory: /tmp/loki/chunks
+      '';
+    };
     nginx = {
       enable = false;
       # TODO: TLS enabled
