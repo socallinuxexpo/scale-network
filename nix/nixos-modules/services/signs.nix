ed68b48129c929485789d2fd3e7f87ca83815641
Daniel Baker <daniel.n.baker@gmail.com>
2025-02-19 23:21:17 -0800

    nixosModules.services.signs: remove nginx definitions
    Trying to setup signs with nginx causes conflicts with monitoring.
    Removing the configuration for now until we can resolve the nginx setup.

--- nix/nixos-modules/services/signs.nix
+++ nix/nixos-modules/services/signs.nix
@@ -13,10 +13,6 @@ let
   inherit (lib.options)
     mkEnableOption
     ;
-
-
-  # TODO(conjones) make an option with a default
-  hostname = "monitoring.scale.lan";
 in
 {
   options.scale-network.services.signs.enable = mkEnableOption "SCaLE signs service";
@@ -27,19 +23,10 @@ in
       containers.scale-signs = {
         environmentFiles = [ /var/lib/secrets/scale-sign-secrets.env ];
         image = "sarcasticadmin/scale-signs:1a4fbab";
-        ports = [ "8080:80" ];
+        ports = [ "80:80" ];
         extraOptions = [
           "--network=host"
         ];
-      };
-    };
-    virtualHosts."${hostname}" = {
-      # ACME wont work for us on the private network
-      enableACME = false;
-      locations."/" = {
-	# TODO(conjones) grab port from config
-        proxyPass = "http://localhost:8080";
-        proxyWebsockets = true;
       };
     };
   };
