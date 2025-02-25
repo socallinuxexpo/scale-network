075b883c762d3b5c949aa68ee025485745af408b
Daniel Baker <daniel.n.baker@gmail.com>
2025-02-24 20:38:25 -0800

    checks.monitor: add test to check routing for grafana
    Co-authored-by: Robert James Hernandez <rob@sarcasticadmin.com>

--- nix/checks/monitor.nix
+++ nix/checks/monitor.nix
@@ -13,13 +13,30 @@
     virtualisation.graphics = true;
   };
 
-  testScript = ''
-    start_all()
-    coremaster.succeed("sleep 2")
-    coremaster.wait_for_unit("grafana.service", None, 30)
-    coremaster.wait_for_unit("prometheus.service", None, 30)
-    coremaster.wait_until_succeeds("nc -vz localhost 3000")
-  '';
+  nodes.client1 =
+    { pkgs, ... }:
+    {
+      systemd.services.systemd-networkd.environment.SYSTEMD_LOG_LEVEL = "debug";
+      #networking.extraHosts = "IP monitoring.scale.lan";
+      environment = {
+        systemPackages = with pkgs; [
+          curl
+        ];
+      };
+    };
+
+  testScript =
+    { nodes, ... }:
+    ''
+      start_all()
+      coremaster.succeed("sleep 2")
+      coremaster.wait_for_unit("grafana.service", None, 30)
+      coremaster.wait_for_unit("prometheus.service", None, 30)
+      coremaster.wait_until_succeeds("nc -vz localhost 3000")
+
+      client1.wait_until_succeeds("ping -c 5 ${nodes.coremaster.networking.hostName}")
+      client1.wait_until_succeeds("curl -v -L -H \"Host: monitoring.scale.lan\" http://${nodes.coremaster.networking.hostName}")
+    '';
 
   # TODO:
   # - Create machine that replays AP data
