4edb94babea47827fe54466ce3d489acc17c30cc
Jeffrey Jung <jeffreytjung@gmail.com>
2025-02-28 17:50:49 -0800

    correct port on the loopback in promtail in configuration.nix

---
+++ nix/nixos-configurations/monitor/configuration.nix
@@ -0,0 +1,152 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  hostname = "monitoring.scale.lan";
+  dashboard = pkgs.copyPathToStore ../../../monitoring/openwrt_dashboard.json;
+in
+{
+  boot.kernelParams = [
+    "console=ttyS0"
+    "boot.shell_on_fail"
+  ];
+
+  systemd.network = {
+    enable = true;
+    networks = {
+      "10-lan" = {
+        # to match enp0 or eth0
+        name = "e*0*";
+        enable = true;
+        address = [
+          "10.0.3.6/24"
+          "2001:470:f026:103::6"
+        ];
+        routes = [
+          { routeConfig.Gateway = "10.0.3.1"; }
+          { routeConfig.Gateway = "2001:470:f026:103::1"; }
+        ];
+      };
+    };
+  };
+  networking.hostName = "monitor";
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
+  # TODO: How to handle sudo esculation
+  security.sudo.wheelNeedsPassword = false;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    bintools
+  ];
+
+  services = {
+    prometheus = {
+      enable = true;
+      enableReload = true;
+      scrapeConfigs = [
+        {
+          job_name = "prometheus";
+          static_configs = [
+            {
+              targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
+              labels = {
+                instance = "localhost";
+              };
+            }
+          ];
+        }
+        {
+          job_name = "ap";
+          static_configs = builtins.fromJSON (
+            builtins.readFile "${pkgs.scale-network.scaleInventory}/config/prom.json"
+          );
+        }
+      ];
+    };
+
+    grafana = {
+      enable = true;
+      settings = {
+        server = {
+          http_addr = "127.0.0.1";
+          http_port = 3000;
+          domain = "${hostname}";
+        };
+        analytics.reporting_enabled = false;
+      };
+      provision = {
+        # Can use just datasources anymore
+        # https://github.com/NixOS/nixpkgs/blob/41de143fda10e33be0f47eab2bfe08a50f234267/nixos/modules/services/monitoring/grafana.nix#L101-L104
+        datasources.settings.datasources = [
+          {
+            name = "prometheus";
+            uid = "P1809F7CD0C75ACF3";
+            type = "prometheus";
+            access = "proxy";
+            url = "http://127.0.0.1:${toString config.services.prometheus.port}";
+          }
+        ];
+        dashboards.settings.providers = [
+          {
+            name = "openwrt";
+            options.path = dashboard;
+          }
+        ];
+      };
+    };
+    
+    services.promtail = {
+      enable = true;
+      configuration = {
+        server = {
+          http_listen_port = 3300;
+          grpc_listen_port = 0;
+        };
+        positions = {
+          filename = "/tmp/positions.yaml";
+        };
+        clients = [{
+          url = "http://127.0.0.1:3100/loki/api/v1/push";
+        }];
+        scrape_configs = [{
+          job_name = "journal";
+          journal = {
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+              host = "pihole";
+            };
+          };
+          relabel_configs = [{
+            source_labels = [ "__journal__systemd_unit" ];
+            target_label = "unit";
+          }];
+        }];
+      };
+    };
+    
+    nginx = {
+      enable = false;
+      # TODO: TLS enabled
+      # Good example enable TLS, but would like to keep it out of the /nix/store
+      # ref: https://github.com/NixOS/nixpkgs/blob/c6fd903606866634312e40cceb2caee8c0c9243f/nixos/tests/custom-ca.nix#L80
+      virtualHosts."${hostname}" = {
+        default = true;
+        # ACME wont work for us on the private network
+        enableACME = false;
+        locations."/" = {
+          proxyPass = "http://${toString config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port}/";
+          proxyWebsockets = true;
+        };
+      };
+    };
+  };
+}
