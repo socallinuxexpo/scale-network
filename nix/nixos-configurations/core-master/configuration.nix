b5af0c2159e7729a7a57e49e31fe6150f9078c3c
Robert James Hernandez <rob@sarcasticadmin.com>
2025-03-01 05:42:18 +0000

    nixosConfigurations.coreMaster: bare metal networking
    Previously the networking config was specific to core running as a vm.
    In this case its going to take the bulk of configuration originally
    found in nixosConfigurations.hypervisor1.

--- nix/nixos-configurations/core-master/configuration.nix
+++ nix/nixos-configurations/core-master/configuration.nix
@@ -1,14 +1,8 @@
 {
-  config,
   ...
 }:
 {
 
-  scale-network.facts = {
-    ipv4 = "10.128.3.20/24";
-    ipv6 = "2001:470:f026:503::20/64";
-    eth = "eth0";
-  };
   scale-network.services.keaMaster.enable = true;
   scale-network.services.bindMaster.enable = true;
   scale-network.services.ntp.enable = true;
@@ -16,36 +10,5 @@
   scale-network.services.signs.enable = true;
   scale-network.services.monitoring.enable = true;
 
-  networking.hostName = "coremaster";
-
-  # disable legacy networking bits as recommended by:
-  #  https://github.com/NixOS/nixpkgs/issues/10001#issuecomment-905532069
-  #  https://github.com/NixOS/nixpkgs/blob/82935bfed15d680aa66d9020d4fe5c4e8dc09123/nixos/tests/systemd-networkd-dhcpserver.nix
-  networking = {
-    extraHosts = ''
-      10.128.3.20 coreconf.scale.lan
-    '';
-  };
-
-  # Make sure that the nix/machines/core/master.nixmakes of these files are actually lexicographically before 99-default.link provides by systemd defaults since first match wins
-  # Ref: https://github.com/systemd/systemd/issues/9227#issuecomment-395500679
-  networking.useDHCP = false;
-  systemd.network = {
-    enable = true;
-    networks = {
-      "10-lan" = {
-        # to match enp0 or eth0
-        name = "e*0*";
-        enable = true;
-        address = [
-          config.scale-network.facts.ipv4
-          config.scale-network.facts.ipv6
-        ];
-        routes = [
-          { routeConfig.Gateway = "10.128.3.1"; }
-          { routeConfig.Gateway = "2001:470:f026:503::1"; }
-        ];
-      };
-    };
   };
 }
--- nix/nixos-configurations/core-master/default.nix
+++ nix/nixos-configurations/core-master/default.nix
@@ -3,8 +3,15 @@
 
   modules =
     {
+      config,
+      lib,
       ...
     }:
+    let
+      inherit (lib.modules)
+        mkForce
+        ;
+    in
     {
       imports = [
         ./configuration.nix
@@ -30,7 +37,76 @@
           users.root.enable = true;
           users.ruebenramirez.enable = true;
         };
+        scale-network.facts = {
+          ipv4 = "10.128.3.20/24";
+          ipv6 = "2001:470:f026:503::20/64";
+          eth = "virbr0";
+        };
 
+        networking.hostName = "coremaster";
+
+        # disable legacy networking bits as recommended by:
+        #  https://github.com/NixOS/nixpkgs/issues/10001#issuecomment-905532069
+        #  https://github.com/NixOS/nixpkgs/blob/82935bfed15d680aa66d9020d4fe5c4e8dc09123/nixos/tests/systemd-networkd-dhcpserver.nix
+        networking = {
+          extraHosts = ''
+            10.128.3.20 coreconf.scale.lan
+          '';
+        };
+
+        systemd.services.systemd-networkd-wait-online.enable = mkForce false;
+        # Make sure that the nix/machines/core/master.nixmakes of these files are actually lexicographically before 99-default.link provides by systemd defaults since first match wins
+        # Ref: https://github.com/systemd/systemd/issues/9227#issuecomment-395500679
+        networking.useDHCP = false;
+        systemd.network = {
+          enable = true;
+
+          netdevs.virbr0.netdevConfig = {
+            Kind = "bridge";
+            Name = "virbr0";
+          };
+
+          networks = {
+            "1-virbr0" = {
+              matchConfig.Name = "virbr0";
+              enable = true;
+              address = [
+                config.scale-network.facts.ipv4
+                config.scale-network.facts.ipv6
+              ];
+              routes = [
+                { routeConfig.Gateway = "10.128.3.1"; }
+                { routeConfig.Gateway = "2001:470:f026:503::1"; }
+              ];
+            };
+            "10-lan-eno2" = {
+              matchConfig.Name = "eno2";
+              networkConfig = {
+                Bridge = "virbr0";
+                LLDP = true;
+                EmitLLDP = true;
+              };
+            };
+            "10-lan-eno3" = {
+              matchConfig.Name = "eno3";
+              networkConfig = {
+                Bridge = "virbr0";
+                LLDP = true;
+                EmitLLDP = true;
+              };
+            };
+            # Keep this for troubleshooting
+            "10-lan-eno1" = {
+              matchConfig.Name = "eno1";
+              enable = true;
+              networkConfig = {
+                DHCP = "yes";
+                LLDP = true;
+                EmitLLDP = true;
+              };
+            };
+          };
+        };
       };
     };
 }
