2e2fce603d65075690fbd3d960db7095d5471be0
Daniel Baker <daniel.n.baker@gmail.com>
2025-03-02 00:42:06 -0800

    checks.wasgeht: init
    Add check for the wasgeht services.
    
    (cherry picked from commit 035d5a3630bd8b9053206803d310f9781cb31ca1)
    
    Co-authored-by: kylerisse <kylerisse@users.noreply.github.com>

---
+++ nix/checks/wasgeht.nix
@@ -0,0 +1,44 @@
+{ inputs }:
+{
+  name = "wasgeht";
+
+  nodes.coremaster = {
+    _module.args = {
+      inherit inputs;
+    };
+    imports = [
+      inputs.self.nixosModules.default
+    ];
+    virtualisation.graphics = true;
+    scale-network.services.wasgeht.enable = true;
+  };
+
+  nodes.client1 =
+    { pkgs, ... }:
+    {
+      systemd.services.systemd-networkd.environment.SYSTEMD_LOG_LEVEL = "debug";
+      environment = {
+        systemPackages = with pkgs; [
+          curl
+        ];
+      };
+    };
+
+  testScript =
+    { nodes, ... }:
+    ''
+      start_all()
+      coremaster.wait_for_unit("wasgeht.service", None, 30)
+      coremaster.succeed("nc -vz localhost 1982")
+      coremaster.succeed("curl -v --fail http://localhost:1982")
+      coremaster.succeed("curl -v --fail http://localhost:1982/imgs")
+      coremaster.succeed("curl -v --fail http://localhost:1982/api")
+      coremaster.wait_until_succeeds("test -f /persist/var/lib/wasgeht/rrds/localhost_latency.rrd")
+      coremaster.wait_until_succeeds("journalctl -u wasgeht --no-pager | grep localhost | grep 'Ping successful'")
+      coremaster.wait_until_succeeds("test -f /persist/var/lib/wasgeht/graphs/imgs/localhost/localhost_latency_15m.png")
+
+      client1.succeed("curl -v --fail http://${nodes.coremaster.networking.hostName}:1982")
+      client1.succeed("curl -q http://${nodes.coremaster.networking.hostName}:1982/api | grep true")
+      client1.succeed("curl -v --fail http://${nodes.coremaster.networking.hostName}:1982/imgs/localhost/localhost_latency_15m.png")
+    '';
+}
