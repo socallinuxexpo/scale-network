883d2f644262e44819b38ff64bea4294a2087202
kylerisse <kylerisse@users.noreply.github.com>
2025-03-04 12:59:55 -0800

    nix.checks.loghost: add tests for consolidated messages file

--- nix/checks/loghost.nix
+++ nix/checks/loghost.nix
@@ -20,7 +20,9 @@
     coremaster.succeed("systemctl is-active syslog")
     coremaster.succeed("logger -n 127.0.0.1 -P 514 --tcp 'troyTCP'")
     coremaster.succeed("cat /persist/rsyslog/**/root.log | grep troyTCP")
+    coremaster.succeed("cat /persist/rsyslog/**/messages.log | grep troyTCP")
     coremaster.succeed("logger -n 127.0.0.1 -P 514 --udp 'troyUDP'")
     coremaster.succeed("cat /persist/rsyslog/**/root.log | grep troyUDP")
+    coremaster.succeed("cat /persist/rsyslog/**/messages.log | grep troyUDP")
   '';
 }
