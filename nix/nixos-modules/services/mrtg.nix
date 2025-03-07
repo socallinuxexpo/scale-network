05fe11b11f7d912e08718e9078ef0d04828ca885
Robert James Hernandez <rob@sarcasticadmin.com>
2025-03-07 04:23:31 +0000

    mrtg timers

--- nix/nixos-modules/services/mrtg.nix
+++ nix/nixos-modules/services/mrtg.nix
@@ -44,52 +44,85 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
-    systemd.services.mrtg-generator =
-      let
-        unfilteredList = (
-          builtins.split "\n" (
-            builtins.readFile "${pkgs.scale-network.scaleInventory}/config/all-network-devices"
-          )
-        );
-        filteredList = (builtins.filter (line: line != [ ] && line != "") unfilteredList);
-        script = pkgs.writeShellScript "" ''
-          set -x
-          mkdir -p ${cfg.statePath}/configs
-          mkdir -p ${cfg.statePath}/graphs
-          for hostname in ${toString filteredList}; do
-            echo "''${hostname}"
-            ${pkgs.mrtg}/bin/cfgmaker \
-            --enable-ipv6 \
-            --no-down \
-            --show-op-down \
-            --output="${cfg.statePath}/configs/''${hostname}.cfg" \
-            --global="WorkDir: ${cfg.statePath}/graphs/''${hostname}/" \
-            Junitux@''${hostname}
-          done
-        '';
-      in
+  config =
+    let
+      unfilteredList = (
+        builtins.split "\n" (
+          builtins.readFile "${pkgs.scale-network.scaleInventory}/config/all-network-devices"
+        )
+      );
+      filteredList = (builtins.filter (line: line != [ ] && line != "") unfilteredList);
+      script = pkgs.writeShellScript "mrtg-generator" ''
+        set -x
+        mkdir -p ${cfg.statePath}/configs
+        mkdir -p ${cfg.statePath}/graphs
+        for hostname in ${toString filteredList}; do
+          echo "''${hostname}"
+          ${pkgs.mrtg}/bin/cfgmaker \
+          --enable-ipv6 \
+          --no-down \
+          --show-op-down \
+          --output="${cfg.statePath}/configs/''${hostname}.cfg" \
+          --global="WorkDir: ${cfg.statePath}/graphs/''${hostname}/" \
+          Junitux@''${hostname}
+        done
+      '';
+    in
+    mkIf cfg.enable (lib.mkMerge [
       {
-        description = "mrtg generator";
-        after = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          User = "${cfg.user}";
-          Group = "${cfg.group}";
-          ExecStart = "${script}";
-          Type = "simple";
+        systemd.services = builtins.listToAttrs (
+          map (name: {
+            name = "${name}-mrtg";
+            value = {
+              after = [ "mrtg-generator.service" ];
+              serviceConfig = {
+                ExecStart = "${pkgs.mrtg}/bin/mrtg ${cfg.statePath}/configs/${name}";
+                User = "${cfg.user}";
+                Group = "${cfg.group}";
+                Type = "simple";
+              };
+            };
+          }) filteredList
+        );
+        systemd.timers = builtins.listToAttrs (
+          map (name: {
+            name = "${name}-mrtg";
+            value = {
+              wantedBy = [ "timers.target" ];
+              timerConfig = {
+                OnBootSec = "5m";
+                OnUnitActiveSec = "5m";
+                Unit = "${name}-mrtg.service";
+              };
+            };
+          }) filteredList
+        );
+      }
+      {
+        systemd.services.mrtg-generator = {
+          description = "mrtg generator";
+          after = [ "network.target" ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            User = "${cfg.user}";
+            Group = "${cfg.group}";
+            ExecStart = "${script}";
+            Type = "simple";
+          };
         };
-      };
-    systemd.tmpfiles.rules = [
-      "d ${cfg.statePath} 0755 ${cfg.user} ${cfg.group}"
-    ];
+        systemd.tmpfiles.rules = [
+          "d ${cfg.statePath} 0755 ${cfg.user} ${cfg.group}"
+        ];
 
-    users = {
-      users."${cfg.user}" = {
-        isNormalUser = true;
-        group = "${cfg.group}";
-      };
-      groups.${cfg.group} = { };
-    };
-  };
+        #mrtg ${cfg.statePath}/configs/${hostname}
+
+        users = {
+          users."${cfg.user}" = {
+            isNormalUser = true;
+            group = "${cfg.group}";
+          };
+          groups.${cfg.group} = { };
+        };
+      }
+    ]);
 }
