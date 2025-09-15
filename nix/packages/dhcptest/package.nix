82d54cbc29557d691972366fe0ce97c36446bc99
Robert James Hernandez <rob@sarcasticadmin.com>
2025-09-15 22:13:05 +0000

    dhcptest: init at 0.9-unstable-2024-03-20

---
+++ nix/packages/dhcptest/package.nix
@@ -0,0 +1,27 @@
+{
+  stdenv,
+  dmd,
+  fetchFromGitHub,
+}:
+
+stdenv.mkDerivation {
+  pname = "dhcptest";
+  version = "0.9-unstable-2024-03-20";
+
+  src = fetchFromGitHub {
+    owner = "CyberShadow";
+    repo = "dhcptest";
+    rev = "4807603943e7ab984964ad549aeb8b63e0b0b5a2";
+    hash = "sha256-KeICp8rum550lFNbeArWyCff9sDd9nxrbltVXi7yBvI=";
+  };
+
+  nativeBuildInputs = [ dmd ];
+
+  buildPhase = ''
+    dmd dhcptest.d
+  '';
+
+  installPhase = ''
+    install -Dt $out/bin dhcptest
+  '';
+}
