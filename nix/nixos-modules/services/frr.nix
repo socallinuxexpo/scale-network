5fe6a4ae639c48f8dd1e25d27daf9bfc2e071020
Ryan Hamel <ryan@rkhtech.org>
2025-11-06 01:35:06 -0600

    Add v6 OSPF to frr.nix

--- nix/nixos-modules/services/frr.nix
+++ nix/nixos-modules/services/frr.nix
@@ -51,6 +51,10 @@ in
          network 10.0.0.0/8 area 0
          redistribute connected
         exit
+        router ospf6
+         passive-interface default
+         redistribute connected
+        exit
       '';
     };
 
@@ -77,6 +81,10 @@ in
                ip ospf network broadcast
                ip ospf hello-interval 1
                ip ospf dead-interval 3
+               no ipv6 ospf6 passive
+               ipv6 ospf6 network broadcast
+               ipv6 ospf6 hello-interval 1
+               ipv6 ospf6 dead-interval 3
               exit
             '') cfg.broadcast-interface
           );
