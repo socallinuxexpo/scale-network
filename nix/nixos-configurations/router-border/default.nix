56104af1bc03ea0737e89629a9662d0f35ddaea7
Robert James Hernandez <rob@sarcasticadmin.com>
2025-11-06 23:18:37 +0000

    nixos-configurations.router-border: setup nftables nat ruleset

--- nix/nixos-configurations/router-border/default.nix
+++ nix/nixos-configurations/router-border/default.nix
@@ -21,6 +21,28 @@
           "net.ipv6.conf.all.forwarding" = true;
         };
 
+        networking.nftables.enable = true;
+        networking.nftables.ruleset = ''
+           table ip nat {
+            chain PREROUTING {
+              type nat hook prerouting priority dstnat; policy accept;
+            }
+
+            chain INPUT {
+              type nat hook input priority 100; policy accept;
+            }
+
+            chain OUTPUT {
+              type nat hook output priority -100; policy accept;
+            }
+
+            chain POSTROUTING {
+              type nat hook postrouting priority srcnat; policy accept;
+              oifname "copper0" ip daddr 0.0.0.0/0 counter masquerade
+            }
+          }
+        '';
+
         nixpkgs.hostPlatform = "x86_64-linux";
         networking.hostName = "router-border";
         # make friend eth names based on paths from lspci
