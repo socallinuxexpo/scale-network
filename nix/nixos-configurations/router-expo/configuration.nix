5fb063c350cf50400660bde858a6c711c41dc0be
Robert James Hernandez <rob@sarcasticadmin.com>
2025-11-06 01:19:37 +0000

    nixosConfigs.router-expo: init

---
+++ nix/nixos-configurations/router-expo/configuration.nix
@@ -0,0 +1,23 @@
+{
+  ...
+}:
+{
+  scale-network = {
+    base.enable = true;
+    services.frr.enable = true;
+    services.frr.router-id = "10.1.2.3";
+    services.frr.broadcast-interface = [
+      "fiber0"
+      "fiber1"
+    ];
+    services.ssh.enable = true;
+
+    users.conjones.enable = true;
+    users.kylerisse.enable = true;
+    users.owen.enable = true;
+    users.rhamel.enable = true;
+    users.rob.enable = true;
+    users.root.enable = true;
+    users.erikreinert.enable = true;
+  };
+}
---
+++ nix/nixos-configurations/router-expo/default.nix
@@ -0,0 +1,74 @@
+{
+  release = "unstable";
+
+  modules =
+    {
+      config,
+      lib,
+      ...
+    }:
+    {
+      imports = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+        ./disko.nix
+      ];
+
+      config = {
+        nixpkgs.hostPlatform = "x86_64-linux";
+        networking.hostName = "router-expo";
+        # make friend eth names based on paths from lspci
+        services.udev.extraRules = ''
+          # Ethernet controller: Realtek Semiconductor Co., Ltd. RTL8111/8168/8211/8411 PCI Express Gigabit Ethernet Controller (rev 15)
+          SUBSYSTEM=="net", ACTION=="add", KERNELS=="0000:11:00.0", NAME="backdoor0"
+          # Ethernet controller: Intel Corporation 82599ES 10-Gigabit SFI/SFP+ Network Connection (rev 01)
+          SUBSYSTEM=="net", ACTION=="add", KERNELS=="0000:03:00.0", NAME="fiber0"
+          SUBSYSTEM=="net", ACTION=="add", KERNELS=="0000:03:00.1", NAME="fiber1"
+          SUBSYSTEM=="net", ACTION=="add", KERNELS=="0000:06:00.0", NAME="fiber2"
+          SUBSYSTEM=="net", ACTION=="add", KERNELS=="0000:06:00.1", NAME="fiber3"
+          # Ethernet controller: Intel Corporation I350 Gigabit Network Connection (rev 01)
+          SUBSYSTEM=="net", ACTION=="add", KERNELS=="0000:0d:00.0", NAME="copper0"
+          SUBSYSTEM=="net", ACTION=="add", KERNELS=="0000:0d:00.1", NAME="copper1"
+          SUBSYSTEM=="net", ACTION=="add", KERNELS=="0000:0d:00.2", NAME="copper2"
+          SUBSYSTEM=="net", ACTION=="add", KERNELS=="0000:0d:00.3", NAME="copper3"
+        '';
+
+        # must be disabled if using systemd.network
+        networking.useDHCP = false;
+
+        systemd.network = {
+          enable = true;
+
+          networks = {
+            # Keep this for troubleshooting
+            "10-backdoor" = {
+              matchConfig.Name = "backdoor0";
+              enable = true;
+              networkConfig = {
+                DHCP = "yes";
+                LLDP = true;
+                EmitLLDP = true;
+              };
+              linkConfig.RequiredForOnline = "no";
+            };
+            # Physical link to border
+            "10-cf" = {
+	      matchConfig.Name = "fiber0";
+	      networkConfig.DHCP = false;
+	      address = [
+		"10.1.2.3/24"
+	      ];
+	    };
+            # Physical link to conference
+            "10-expo" = {
+	      matchConfig.Name = "fiber1";
+	      networkConfig.DHCP = false;
+	      address = [
+		"10.1.3.3/24"
+	      ];
+	    };
+          };
+        };
+      };
+    };
+}
