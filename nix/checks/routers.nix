c55dcad4189424cb5f8c0a348b3e44def98486c5
Robert James Hernandez <rob@sarcasticadmin.com>
2025-12-11 04:13:12 +0000

    treewide: border router module

--- nix/checks/routers.nix
+++ nix/checks/routers.nix
@@ -18,49 +18,29 @@
           inputs.self.nixosModules.default
         ];
         virtualisation.vlans = [
+          10 # NAT
           2 # border <-> conference
           3 # border <-> expo
-          10 # NAT
         ];
-
-        services.udev.extraRules = ''
-          SUBSYSTEM=="net", ACTION=="add", KERNELS=="0000:00:0b.0", NAME="fiber0"
-          SUBSYSTEM=="net", ACTION=="add", KERNELS=="0000:00:0c.0", NAME="fiber1"
-        '';
 
         scale-network = {
           base.enable = true;
-          router.border.enable = true;
+          router.border = {
+            enable = true;
+            staticWANEnable = true;
+            WANInterface = "eth1";
+            broadcastInterfaces = [
+              "eth2"
+              "eth3"
+            ];
+          };
           services.frr.enable = true;
           services.frr.router-id = "10.1.1.1";
           services.frr.broadcast-interface = [
-            "eth1"
             "eth2"
+            "eth3"
           ];
         };
-        networking.nftables.enable = true;
-        networking.nftables.ruleset = ''
-           table ip nat {
-            chain PREROUTING {
-              type nat hook prerouting priority dstnat; policy accept;
-            }
-
-            chain INPUT {
-              type nat hook input priority 100; policy accept;
-            }
-
-            chain OUTPUT {
-              type nat hook output priority -100; policy accept;
-            }
-
-            chain POSTROUTING {
-              type nat hook postrouting priority srcnat; policy accept;
-              oifname "eth3" ip daddr 0.0.0.0/0 counter masquerade
-            }
-          }
-        '';
-        networking.firewall.enable = false;
-
       };
     conference =
       { ... }:
--- nix/nixos-modules/routers/border.nix
+++ nix/nixos-modules/routers/border.nix
@@ -12,10 +12,42 @@ let
 
   inherit (lib.options)
     mkEnableOption
+    mkOption
     ;
+
+  inherit (lib)
+    types
+    mkMerge
+    ;
+
+  inherit (lib.lists)
+    head
+    tail
+    ;
+
 in
 {
-  options.scale-network.router.border.enable = mkEnableOption "SCaLE network border router setup";
+  options.scale-network.router.border = {
+    enable = mkEnableOption "SCaLE network border router setup";
+    staticWANEnable = mkEnableOption "WAN Interface static IP";
+    WANInterface = mkOption {
+      type = types.str;
+      default = "copper0";
+      description = ''
+        Internet goes here
+      '';
+    };
+    broadcastInterfaces = mkOption {
+      type = types.listOf types.str;
+      default = [
+        "fiber0"
+        "fiber1"
+      ];
+      description = ''
+        FRR interfaces
+      '';
+    };
+  };
 
   config = mkIf cfg.enable {
     networking.useNetworkd = true;
@@ -26,37 +58,80 @@ in
       "net.ipv6.conf.all.forwarding" = true;
     };
 
-    # Physical link to Internet
-    systemd.network.networks."10-inet" = {
-      matchConfig.Name = "eth0";
-      networkConfig.DHCP = true;
-    };
-    # Physical link to conference center
-    systemd.network.networks."10-cf" = {
-      matchConfig.Name = "eth1";
-      networkConfig.DHCP = false;
-      address = [
-        "10.1.1.1/24"
-      ];
-    };
-    # Physical link to expo hall
-    systemd.network.networks."10-expo" = {
-      matchConfig.Name = "eth2";
-      networkConfig.DHCP = false;
-      address = [
-        "10.1.2.1/24"
-      ];
-    };
-    # Physical link to cf Firewall
-    systemd.network.networks."10-cffw" = {
-      matchConfig.Name = "eth3";
-      networkConfig.DHCP = false;
-      address = [
-        "172.16.1.1/24"
+    # must be disabled if using systemd.network
+    networking.useDHCP = false;
+
+    systemd.network = {
+      enable = true;
+
+      networks = mkMerge [
+        {
+          # Physical link to conference center
+          "10-cf" = {
+            matchConfig.Name = head cfg.broadcastInterfaces;
+            networkConfig.DHCP = false;
+            address = [
+              "10.1.1.1/24"
+            ];
+          };
+          # Physical link to expo
+          "10-expo" = {
+            matchConfig.Name = tail cfg.broadcastInterfaces;
+            networkConfig.DHCP = false;
+            address = [
+              "10.1.2.1/24"
+            ];
+          };
+        }
+        (mkIf (!cfg.staticWANEnable) {
+          # temporary for testing at various sites
+          # will be static for show
+          "10-nat-dhcp" = {
+            matchConfig.Name = cfg.WANInterface;
+            enable = true;
+            networkConfig = {
+              DHCP = "yes";
+              LLDP = true;
+              EmitLLDP = true;
+            };
+            linkConfig.RequiredForOnline = "no";
+          };
+        })
+
+        (mkIf cfg.staticWANEnable {
+          "10-nat-static" = {
+            matchConfig.Name = cfg.WANInterface;
+            networkConfig.DHCP = false;
+            address = [
+              "172.16.1.1/24"
+            ];
+          };
+        })
       ];
     };
 
-    networking.nftables.ruleset = "";
+    networking.firewall.enable = false;
+    networking.nftables.enable = true;
+    networking.nftables.ruleset = ''
+       table ip nat {
+        chain PREROUTING {
+          type nat hook prerouting priority dstnat; policy accept;
+        }
+
+        chain INPUT {
+          type nat hook input priority 100; policy accept;
+        }
+
+        chain OUTPUT {
+          type nat hook output priority -100; policy accept;
+        }
+
+        chain POSTROUTING {
+          type nat hook postrouting priority srcnat; policy accept;
+          oifname "${cfg.WANInterface}" ip daddr 0.0.0.0/0 counter masquerade
+        }
+      }
+    '';
 
     system.stateVersion = "25.11";
   };
