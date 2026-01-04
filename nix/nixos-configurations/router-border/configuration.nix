f667b56e142cb258355b25ed5e2d9097c80c7c14
Robert James Hernandez <rob@sarcasticadmin.com>
2026-01-04 00:40:13 +0000

    nixos-*.router-border: enable static IP and routes
    Hardcoding these for testing existing network

--- nix/nixos-configurations/router-border/configuration.nix
+++ nix/nixos-configurations/router-border/configuration.nix
@@ -6,7 +6,7 @@
     base.enable = true;
     router.border = {
       enable = true;
-      staticWANEnable = false;
+      staticWANEnable = true;
       WANInterface = "copper0";
       frrConferenceInterface = "fiber0";
       frrExpoInterface = "fiber1";
