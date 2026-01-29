e7958a9e534467dd301b078ae02bbe84ab96749e
Robert James Hernandez <rob@sarcasticadmin.com>
2026-01-29 04:18:34 +0000

    nix-modules.services.dhcp{4-6}-relay: init
    running isc-dhcp-relay for ipv4 and ipv6 with support for multiple
    instances.

---
+++ nix/nixos-modules/services/dhcp4-relay.nix
@@ -0,0 +1,94 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+
+  inherit (lib)
+    types
+    ;
+
+  inherit (lib.strings)
+    concatMapStringsSep
+    concatStringsSep
+    ;
+
+  inherit (lib.modules)
+    mkIf
+    ;
+
+  inherit (lib.options)
+    mkEnableOption
+    mkOption
+    ;
+
+  inherit (lib.attrsets)
+    filterAttrs
+    mapAttrs'
+    ;
+
+  cfg = config.scale-network.services.dhcp4-relay;
+
+  enabledRelays = filterAttrs (relayName: cfg: cfg.enable) cfg;
+
+  relayOpts = {
+
+    options = {
+      enable = mkEnableOption "SCaLE dhcp4-relay v4 monitoring service";
+
+      listenInterfaces = mkOption {
+        type = types.listOf types.str;
+        default = [ ];
+      };
+
+      dhcpServerIps = mkOption {
+        type = types.listOf types.str;
+      };
+    };
+  };
+
+in
+
+{
+  options.scale-network.services.dhcp4-relay = mkOption {
+    type = types.attrsOf (types.submodule relayOpts);
+    default = { };
+    description = "Specification of one or more dhcp4-relays.";
+  };
+
+  config = mkIf (enabledRelays != { }) {
+
+    environment.systemPackages = [ pkgs.scale-network.isc-dhcp ];
+
+    systemd.services = mapAttrs' (relayName: relayCfg: {
+      name = "dhcp4-relay-${relayName}";
+      value = {
+        description = "dhcp4-relay monitoring service for ${relayName}";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" ];
+
+        serviceConfig = {
+          ExecStart = "${pkgs.scale-network.isc-dhcp}/bin/dhcrelay -4 -d --no-pid ${
+            concatMapStringsSep " " (x: "-i ${x}") relayCfg.listenInterfaces
+          } ${concatStringsSep " " relayCfg.dhcpServerIps}";
+          Type = "exec";
+          Restart = "always";
+          DynamicUser = true;
+          AmbientCapabilities = [
+            "CAP_NET_RAW" # to send ICMP messages
+            "CAP_NET_BIND_SERVICE" # to bind on DHCP port (67,68)
+          ];
+        };
+      };
+    }) enabledRelays;
+
+    networking = {
+      firewall.allowedUDPPorts = [
+        67
+        68
+      ];
+    };
+  };
+}
---
+++ nix/nixos-modules/services/dhcp6-relay.nix
@@ -0,0 +1,93 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+let
+
+  inherit (lib)
+    types
+    ;
+
+  inherit (lib.strings)
+    concatMapStringsSep
+    ;
+
+  inherit (lib.modules)
+    mkIf
+    ;
+
+  inherit (lib.options)
+    mkEnableOption
+    mkOption
+    ;
+
+  inherit (lib.attrsets)
+    filterAttrs
+    mapAttrs'
+    ;
+
+  cfg = config.scale-network.services.dhcp6-relay;
+
+  enabledRelays = filterAttrs (relayName: cfg: cfg.enable) cfg;
+
+  relayOpts = {
+
+    options = {
+      enable = mkEnableOption "SCaLE dhcp6-relay monitoring service";
+
+      listenInterfaces = mkOption {
+        type = types.listOf types.str;
+        default = [ ];
+      };
+
+      dhcpServerIps = mkOption {
+        type = types.listOf types.str;
+      };
+    };
+  };
+
+in
+
+{
+  options.scale-network.services.dhcp6-relay = mkOption {
+    type = types.attrsOf (types.submodule relayOpts);
+    default = { };
+    description = "Specification of one or more dhcp6-relays.";
+  };
+
+  config = mkIf (enabledRelays != { }) {
+
+    environment.systemPackages = [ pkgs.scale-network.isc-dhcp ];
+
+    systemd.services = mapAttrs' (relayName: relayCfg: {
+      name = "dhcp6-relay-${relayName}";
+      value = {
+        description = "dhcp6-relay monitoring service for ${relayName}";
+        wantedBy = [ "multi-user.target" ];
+        after = [ "network.target" ];
+
+        serviceConfig = {
+          ExecStart = "${pkgs.scale-network.isc-dhcp}/bin/dhcrelay -6 -d --no-pid ${
+            concatMapStringsSep " " (x: "-l ${x}") relayCfg.listenInterfaces
+          } ${concatMapStringsSep " " (x: "-u ${x}") relayCfg.dhcpServerIps}";
+          Type = "exec";
+          Restart = "always";
+          DynamicUser = true;
+          AmbientCapabilities = [
+            "CAP_NET_RAW" # to send ICMP messages
+            "CAP_NET_BIND_SERVICE" # to bind on DHCP port (546,547)
+          ];
+        };
+      };
+    }) enabledRelays;
+
+    networking = {
+      firewall.allowedUDPPorts = [
+        546
+        547
+      ];
+    };
+  };
+}
