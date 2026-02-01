c7aff772842de35cd2f59215b5d89be64d811ee0
Daniel Baker <dan@djacu.dev>
2026-01-31 22:25:44 -0800

    nix/legacy-packages: use defaultSystems
    Centralizes which systems we expose in the flake outputs.

--- nix/legacy-packages/default.nix
+++ nix/legacy-packages/default.nix
@@ -1,15 +1,8 @@
 inputs:
-inputs.nixpkgs-lib.lib.genAttrs
-  [
-    "aarch64-darwin"
-    "aarch64-linux"
-    "x86_64-darwin"
-    "x86_64-linux"
-  ]
-  (
-    system:
-    import inputs.nixpkgs {
-      inherit system;
-      overlays = [ inputs.self.overlays.default ];
-    }
-  )
+inputs.self.library.systems.defaultSystems (
+  system:
+  import inputs.nixpkgs {
+    inherit system;
+    overlays = [ inputs.self.overlays.default ];
+  }
+)
