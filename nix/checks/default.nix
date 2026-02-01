39300287e2ff195969b7249060cafcd32b6216c6
Daniel Baker <dan@djacu.dev>
2026-02-01 12:49:53 -0800

    nix/checks: migrate all the nixos vm tests to the test package set

--- nix/checks/default.nix
+++ nix/checks/default.nix
@@ -11,14 +11,6 @@ let
 
 in
 mapAttrs (system: pkgs: {
-  core = pkgs.testers.runNixOSTest (import ./core.nix { inherit inputs lib; });
-  routers = pkgs.testers.runNixOSTest (import ./routers.nix { inherit inputs; });
-  router-border = pkgs.testers.runNixOSTest (import ./router-border.nix { inherit inputs lib; });
-  loghost = pkgs.testers.runNixOSTest (import ./loghost.nix { inherit inputs; });
-  monitor = pkgs.testers.runNixOSTest (import ./monitor.nix { inherit inputs; });
-  # impure test and needs to pull container
-  #signs = pkgs.testers.runNixOSTest (import ./signs.nix { inherit inputs; });
-  wasgeht = pkgs.testers.runNixOSTest (import ./wasgeht.nix { inherit inputs; });
 
   formatting = inputs.self.formatterModule.${system}.config.build.check inputs.self;
 
