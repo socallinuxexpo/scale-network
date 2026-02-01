cbea346f0425fa3df0865b49ebf74643b2c2f3a0
Daniel Baker <dan@djacu.dev>
2026-02-01 12:16:32 -0800

    openwrt-golden: move from checks to test package set

--- nix/checks/default.nix
+++ nix/checks/default.nix
@@ -15,16 +15,6 @@ let
     ;
 
   # sources
-
-  # Used for derivations where openwrt is the primary directory.
-  openwrtSrc = toSource {
-    root = ../..;
-    fileset = unions [
-      ../../facts
-      ../../openwrt
-      ../../tests
-    ];
-  };
 in
 mapAttrs (system: pkgs: {
   core = pkgs.testers.runNixOSTest (import ./core.nix { inherit inputs lib; });
@@ -33,27 +23,6 @@ mapAttrs (system: pkgs: {
   loghost = pkgs.testers.runNixOSTest (import ./loghost.nix { inherit inputs; });
   monitor = pkgs.testers.runNixOSTest (import ./monitor.nix { inherit inputs; });
   wasgeht = pkgs.testers.runNixOSTest (import ./wasgeht.nix { inherit inputs; });
-
-  openwrt-golden = pkgs.stdenv.mkDerivation (finalAttrs: {
-    pname = "openwrt-golden";
-    version = "0.1.0";
-
-    src = openwrtSrc;
-
-    buildInputs = [
-      pkgs.diffutils
-      pkgs.gomplate
-    ];
-
-    buildPhase = ''
-      cd tests/unit/openwrt
-      mkdir -p $out/tmp/ath79
-    '';
-
-    installPhase = ''
-      ./test.sh -t ath79 -o $out
-    '';
-  });
 
   formatting = inputs.self.formatterModule.${system}.config.build.check inputs.self;
 
