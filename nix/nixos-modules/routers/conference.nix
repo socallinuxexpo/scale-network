8cafb7249d2e4bf3faffa80c22159e05620de30e
Robert James Hernandez <rob@sarcasticadmin.com>
2026-02-18 08:36:55 +0000

    nixos-modues.router.conference: adding vlans 504-507

--- nix/nixos-modules/routers/conference.nix
+++ nix/nixos-modules/routers/conference.nix
@@ -59,6 +59,10 @@ in
         "501"
         "502"
         "503"
+        "504"
+        "505"
+        "506"
+        "507"
       ];
     };
 
@@ -121,6 +125,62 @@ in
             Name = "bridge503";
           };
         };
+        # cfCTF
+        "20-vlan504" = {
+          netdevConfig = {
+            Kind = "vlan";
+            Name = "vlan504";
+          };
+          vlanConfig.Id = 504;
+        };
+        "25-bridge504" = {
+          netdevConfig = {
+            Kind = "bridge";
+            Name = "bridge504";
+          };
+        };
+        # cfAVLAN Audio Visual
+        "20-vlan505" = {
+          netdevConfig = {
+            Kind = "vlan";
+            Name = "vlan505";
+          };
+          vlanConfig.Id = 505;
+        };
+        "25-bridge505" = {
+          netdevConfig = {
+            Kind = "bridge";
+            Name = "bridge505";
+          };
+        };
+        # cfNOC
+        "20-vlan506" = {
+          netdevConfig = {
+            Kind = "vlan";
+            Name = "vlan506";
+          };
+          vlanConfig.Id = 506;
+        };
+        "25-bridge506" = {
+          netdevConfig = {
+            Kind = "bridge";
+            Name = "bridge506";
+          };
+        };
+        # cfSigns
+        "20-vlan507" = {
+          netdevConfig = {
+            Kind = "vlan";
+            Name = "vlan507";
+          };
+          vlanConfig.Id = 507;
+        };
+        "25-bridge507" = {
+          netdevConfig = {
+            Kind = "bridge";
+            Name = "bridge507";
+          };
+        };
         "25-vlan900" = {
           netdevConfig = {
             Kind = "vlan";
@@ -165,6 +225,10 @@ in
             "vlan501"
             "vlan502"
             "vlan503"
+            "vlan504"
+            "vlan505"
+            "vlan506"
+            "vlan507"
           ];
         };
         "40-vlan500" = {
@@ -223,6 +287,62 @@ in
           address = [
             "10.128.3.1/24"
             "2001:470:f026:503::1/64"
+          ];
+        };
+        "40-vlan504" = {
+          matchConfig.Name = "vlan504";
+          networkConfig = {
+            Bridge = "bridge504";
+          };
+        };
+        "50-bridge504" = {
+          matchConfig.Name = "bridge504";
+          enable = true;
+          address = [
+            "10.128.4.1/24"
+            "2001:470:f026:504::1/64"
+          ];
+        };
+        "40-vlan505" = {
+          matchConfig.Name = "vlan505";
+          networkConfig = {
+            Bridge = "bridge505";
+          };
+        };
+        "50-bridge505" = {
+          matchConfig.Name = "bridge505";
+          enable = true;
+          address = [
+            "10.128.5.1/24"
+            "2001:470:f026:505::1/64"
+          ];
+        };
+        "40-vlan506" = {
+          matchConfig.Name = "vlan506";
+          networkConfig = {
+            Bridge = "bridge506";
+          };
+        };
+        "50-bridge506" = {
+          matchConfig.Name = "bridge506";
+          enable = true;
+          address = [
+            "10.128.6.1/24"
+            "2001:470:f026:506::1/64"
+          ];
+        };
+        "40-vlan507" = {
+          matchConfig.Name = "vlan507";
+          networkConfig = {
+            Bridge = "bridge507";
+          };
+        };
+        "50-bridge507" = {
+          matchConfig.Name = "bridge507";
+          enable = true;
+          address = [
+            "10.128.7.1/24"
+            "2001:470:f026:507::1/64"
           ];
         };
         "30-border" = {
@@ -287,10 +407,14 @@ in
       ];
       services.dhcp4-relay."tech" = {
         enable = true;
+        # excluding bridge507 (cfSigns) since
+        # its a ipv6 only network
         downstreamInterfaces = [
           "bridge500"
           "bridge501"
           "bridge502"
+          "bridge504"
+          "bridge506"
         ];
         upstreamInterfaces = [ "bridge503" ];
         dhcpServerIps = [ "10.128.3.20" ];
